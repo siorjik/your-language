ceee23585b0bbf0183dcabaa3c0319c1ec223549
siorjik <serjkaplun@gmail.com>
2025-08-08 09:02:57 +0300

    some fixes

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -26,7 +26,7 @@ export const dictionaryApiPath = '/api/dictionary'
 export const translateApiPath = '/api/translate'
 
 export const fileUploadApiPath = '/api/files/upload'
-export const fileAuthApiPath = '/api/files/authorize'
+export const fileAuthApiPath = `${appHost}/api/files/authorize`
 
 export const twoFaApiPath = `${appHost}/api/two-fa`
 export const twoFaVerifyApiPath = '/api/two-fa/verify'
@@ -34,4 +34,4 @@ export const twoFaVerifyApiPath = '/api/two-fa/verify'
 export const emailCreatePassApiPath = '/api/emails/create-pass'
 export const emailRecoverPassApiPath = '/api/emails/recover-pass'
 
-export const activityTypesListApiPath = '/api/activity-types'
+export const activityTypesListApiPath = `${appHost}/api/activity-types`
