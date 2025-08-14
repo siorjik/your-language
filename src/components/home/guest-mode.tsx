429f4f5308b9e2e339b658e234bd551b0368b954
siorjik <serjkaplun@gmail.com>
2025-08-14 16:30:30 +0300

    added animation for guest mode

--- src/components/home/guest-mode.tsx
+++ src/components/home/guest-mode.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
+import { easeInOut, motion, useScroll, useTransform } from 'framer-motion'
 
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '../ui/tabs'
 import DialogWrap from '../dialog-wrap'
@@ -16,13 +17,25 @@ import { GlowEffect } from '../ui/glow-effect'
 import flashcardsImg from '@/../public/flashcards.png'
 import deviceImg from '@/../public/devices.png'
 import chartImg from '@/../public/chart.png'
+import useDisplayData from '@/hooks/useDisplayData'
 
 export default function GuestMode() {
   const [isClose, setClose] = useState(false)
 
+  const { isXlDisplay } = useDisplayData()
+
   const { theme } = useTheme()
 
   const isDark = theme?.includes('-dark')
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  const { scrollYProgress } = useScroll({ target: ref, offset: ['end start', 'start end'] })
+
+  const opacity = useTransform(scrollYProgress, [0.5, 1], [1, 0], { ease: [easeInOut] })
+  const y1 = useTransform(scrollYProgress, [0.6, 1], ['0px', '30%'])
+  const y2 = useTransform(scrollYProgress, [0.6, 1], ['0px', '50%'])
+  const y3 = useTransform(scrollYProgress, [0.6, 1], ['0px', '70%'])
 
   const dialogContent = (
     <Tabs>
@@ -70,90 +83,96 @@ export default function GuestMode() {
     <>
       <HeroSection dialogContent={dialogContent} isClose={isClose} />
       <div className="min-h-[calc(100vh-50px)] pt-10 pb-5 flex justify-center items-center">
-        <div className="flex flex-wrap gap-10 justify-center items-center">
-          <Card className="lg:min-h-[850px] flex flex-col justify-between bg-secondary/50 border-0 shadow-xl text-center">
-            <div>
-              <CardHeader>
-                <CardTitle className="font-balsamiqSans text-primary/50">Flashcards & Quizzes</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="max-w-[350px] mx-auto mb-8 p-3 bg-muted/50 rounded-lg italic font-semibold text-muted-foreground">
-                  Master new words and phrases faster than ever with our{' '}
-                  <span className="text-primary">Flashcards & Quizzes</span>. Effortlessly review vocabulary in bite-sized
-                  sessions, then challenge yourself with quick quizzes that keep your memory sharp. Flashcards help you visualize,
-                  repeat, and retain essential language, while quizzes turn learning into an engaging game. Whether you have just
-                  a few minutes or a whole hour, these tools adapt to your pace and keep you motivated. Explore, practice, and
-                  grow your English skills - all in one place.
-                </p>
-                <Image
-                  src={flashcardsImg}
-                  alt="flash-cards"
-                  width={300}
-                  height={300}
-                  className={`mx-auto mb-5 ${isDark ? 'brightness-50' : ''}`}
-                  priority
-                />
-              </CardContent>
-            </div>
-            <CardFooter>
-              <span className="mx-auto">{getDialog('Create Flashcards')}</span>
-            </CardFooter>
-          </Card>
+        <div className="flex flex-wrap gap-10 justify-center items-center" ref={ref}>
+          <motion.div style={{ y: isXlDisplay ? y1 : '', opacity: isXlDisplay ? opacity : 1 }}>
+            <Card className="lg:min-h-[850px] flex flex-col justify-between bg-secondary/50 border-0 shadow-xl text-center">
+              <div>
+                <CardHeader>
+                  <CardTitle className="font-balsamiqSans text-primary/50">Flashcards & Quizzes</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="max-w-[350px] mx-auto mb-8 p-3 bg-muted/50 rounded-lg italic font-semibold text-muted-foreground">
+                    Master new words and phrases faster than ever with our{' '}
+                    <span className="text-primary">Flashcards & Quizzes</span>. Effortlessly review vocabulary in bite-sized
+                    sessions, then challenge yourself with quick quizzes that keep your memory sharp. Flashcards help you
+                    visualize, repeat, and retain essential language, while quizzes turn learning into an engaging game. Whether
+                    you have just a few minutes or a whole hour, these tools adapt to your pace and keep you motivated. Explore,
+                    practice, and grow your English skills - all in one place.
+                  </p>
+                  <Image
+                    src={flashcardsImg}
+                    alt="flash-cards"
+                    width={300}
+                    height={300}
+                    className={`mx-auto mb-5 ${isDark ? 'brightness-50' : ''}`}
+                    priority
+                  />
+                </CardContent>
+              </div>
+              <CardFooter>
+                <span className="mx-auto">{getDialog('Create Flashcards')}</span>
+              </CardFooter>
+            </Card>
+          </motion.div>
 
-          <Card className="lg:min-h-[850px] flex flex-col justify-between bg-secondary/50 border-0 shadow-xl text-center">
-            <div>
-              <CardHeader>
-                <CardTitle className="font-balsamiqSans text-primary/50">Your Activity Tracker</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="max-w-[350px] mx-auto mb-4 p-3 bg-muted/50 rounded-lg italic font-semibold text-muted-foreground">
-                  Stay on top of your progress with <span className="text-primary">Your Activity Tracker</span>. See how much
-                  you`ve learned, track your daily practice, and celebrate every milestone. From completed flashcards to quiz
-                  scores and study streaks, your tracker keeps everything organized in one place. It`s the easiest way to stay
-                  motivated, set goals, and watch your English skills grow step by step. Learning is a journey - let your activity
-                  tracker guide the way!
-                </p>
-                <Image
-                  src={chartImg}
-                  alt="flash-cards"
-                  width={300}
-                  height={300}
-                  className={`mx-auto mb-1 ${isDark ? 'brightness-50' : ''}`}
-                  priority
-                />
-              </CardContent>
-            </div>
-            <CardFooter>
-              <span className="mx-auto">{getDialog('Start Activity')}</span>
-            </CardFooter>
-          </Card>
+          <motion.div style={{ y: isXlDisplay ? y2 : '', opacity: isXlDisplay ? opacity : 1 }}>
+            <Card className="lg:min-h-[850px] flex flex-col justify-between bg-secondary/50 border-0 shadow-xl text-center">
+              <div>
+                <CardHeader>
+                  <CardTitle className="font-balsamiqSans text-primary/50">Your Activity Tracker</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="max-w-[350px] mx-auto mb-4 p-3 bg-muted/50 rounded-lg italic font-semibold text-muted-foreground">
+                    Stay on top of your progress with <span className="text-primary">Your Activity Tracker</span>. See how much
+                    you`ve learned, track your daily practice, and celebrate every milestone. From completed flashcards to quiz
+                    scores and study streaks, your tracker keeps everything organized in one place. It`s the easiest way to stay
+                    motivated, set goals, and watch your English skills grow step by step. Learning is a journey - let your
+                    activity tracker guide the way!
+                  </p>
+                  <Image
+                    src={chartImg}
+                    alt="flash-cards"
+                    width={300}
+                    height={300}
+                    className={`mx-auto mb-1 ${isDark ? 'brightness-50' : ''}`}
+                    priority
+                  />
+                </CardContent>
+              </div>
+              <CardFooter>
+                <span className="mx-auto">{getDialog('Start Activity')}</span>
+              </CardFooter>
+            </Card>
+          </motion.div>
 
-          <Card className="lg:min-h-[850px] flex flex-col justify-between bg-secondary/50 border-0 shadow-xl text-center">
-            <div>
-              <CardHeader>
-                <CardTitle className="font-balsamiqSans text-primary/50">Accessible Anywhere</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="max-w-[350px] mx-auto mb-3 p-3 bg-muted/50 rounded-lg italic font-semibold text-muted-foreground">
-                  With <span className="text-primary font-semibold">Accessible Anywhere</span>, your language learning goes
-                  wherever you go. Whether you`re on your phone, tablet, or computer, all your flashcards, quizzes, and progress
-                  are always synced and ready. Practice on the bus, at the café, or from the comfort of home - your learning is
-                  never limited by location. Just log in and keep moving forward, no matter where life takes you.
-                </p>
-                <Image
-                  src={deviceImg}
-                  alt="flash-cards"
-                  width={300}
-                  height={300}
-                  className={`mx-auto ${isDark ? 'brightness-50' : ''}`}
-                  priority
-                />
-              </CardContent>
-            </div>
-            <CardFooter>
-              <span className="mx-auto">{getDialog('Start Learning')}</span>
-            </CardFooter>
-          </Card>
+          <motion.div style={{ y: isXlDisplay ? y3 : '', opacity: isXlDisplay ? opacity : 1 }}>
+            <Card className="lg:min-h-[850px] flex flex-col justify-between bg-secondary/50 border-0 shadow-xl text-center">
+              <div>
+                <CardHeader>
+                  <CardTitle className="font-balsamiqSans text-primary/50">Accessible Anywhere</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="max-w-[350px] mx-auto mb-3 p-3 bg-muted/50 rounded-lg italic font-semibold text-muted-foreground">
+                    With <span className="text-primary font-semibold">Accessible Anywhere</span>, your language learning goes
+                    wherever you go. Whether you`re on your phone, tablet, or computer, all your flashcards, quizzes, and progress
+                    are always synced and ready. Practice on the bus, at the café, or from the comfort of home - your learning is
+                    never limited by location. Just log in and keep moving forward, no matter where life takes you.
+                  </p>
+                  <Image
+                    src={deviceImg}
+                    alt="flash-cards"
+                    width={300}
+                    height={300}
+                    className={`mx-auto ${isDark ? 'brightness-50' : ''}`}
+                    priority
+                  />
+                </CardContent>
+              </div>
+              <CardFooter>
+                <span className="mx-auto">{getDialog('Start Learning')}</span>
+              </CardFooter>
+            </Card>
+          </motion.div>
         </div>
       </div>
     </>
