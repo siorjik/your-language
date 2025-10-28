75c28cfe9d3e007cc55691ab959e285434dcdb0e
siorjik <serjkaplun@gmail.com>
2025-10-28 11:51:53 +0200

    fixed profile acc-form

--- src/app/(app)/profile/_components/acc-form.tsx
+++ src/app/(app)/profile/_components/acc-form.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import z from 'zod'
 import { useSession, getSession } from 'next-auth/react'
 
@@ -13,16 +12,8 @@ import { SelectedUser } from '@/types/models/user'
 import { Err } from '@/types/errTypes'
 
 export default function EditAccountForm({ isCredentials }: { isCredentials: boolean }) {
-  const [acc, setAcc] = useState<{ name: string; email: string }>({ name: '', email: '' })
-
   const { toast } = useToast()
   const { data: session, update } = useSession()
-
-  useEffect(() => {
-    return () => {
-      if (Object.values(acc).filter((val) => !!val).length) update({ ...acc })
-    }
-  }, [acc])
 
   const submit = async (data: z.infer<typeof updateAccFormTypeSchema>): Promise<boolean> => {
     await getSession()
@@ -37,7 +28,7 @@ export default function EditAccountForm({ isCredentials }: { isCredentials: bool
         description: 'Your account was updated successfully!',
       })
 
-      setAcc({ ...data })
+      update({ ...data })
 
       return true
     } else {
@@ -60,7 +51,7 @@ export default function EditAccountForm({ isCredentials }: { isCredentials: bool
         schema={updateAccFormTypeSchema}
         fieldsData={fieldData}
         btn={{ text: 'Update', css: 'w-auto' }}
-        data={{ email: acc.email || session?.user?.email, name: acc.name || session?.user?.name }}
+        data={{ email: session?.user?.email, name: session?.user?.name }}
         showLoader
       />
     </div>
