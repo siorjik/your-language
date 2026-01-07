bec124382490e800eb854dd49cf898f1d1057565
siorjik <serjkaplun@gmail.com>
2026-01-07 12:02:55 +0200

    returned revalidatePath in activity actions

--- src/actions/activity.ts
+++ src/actions/activity.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { revalidatePath } from 'next/cache'
+import { getLocale } from 'next-intl/server'
+
 import errHandlerService from '@/services/errHandlerService'
 import getServerSessionToken from '@/helpers/getServerSessionToken'
 import { prisma } from '@/lib/prisma'
@@ -9,6 +12,10 @@ import { Err } from '@/types/errTypes'
 export const createActivity = async (activityTypeId: string, setId: string): Promise<(Activity & { error: null }) | Err> => {
   try {
     const session = await getServerSessionToken()
+
+    const locale = await getLocale()
+
+    revalidatePath(`/${locale}`)
 
     return { ...(await prisma.activity.create({ data: { setId, activityTypeId, userId: session.id } })), error: null }
   } catch (error) {
